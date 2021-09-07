b5af63c950b4c0f61b4b48ddac5a9d2ef355323f
Rémi Remy <meltingpot54@gmail.com>
2021-09-07 15:57:13 +0200

    alerte arrêté

--- javascript/pacman.js
+++ javascript/pacman.js
@@ -4,7 +4,7 @@
 let pacman = {
     x: 5,
     y: 2,
-    direction: 0
+    direction: 1
 };
 
 let greenGhost = {
@@ -12,6 +12,12 @@ let greenGhost = {
     y: 2,
     direction: 0
 };
+
+let score = 0
+
+let totalBonbon = 0
+
+let stop = null
 
 let grille = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -40,17 +46,19 @@ let grille = [
 let plateau
 window.addEventListener("load", () => {
     plateau = document.querySelector(".plateau");
-    setInterval(tourDeJeu, 500)
+    stop = setInterval(tourDeJeu, 200)
+    document.body.addEventListener("keyup", clavier) // permet de récupérer la saisie sur le clavier
 
 
 });
 
 function afficheGrille() {
     plateau.innerHTML = ""  // permet de supprimer à chaque rafraichissement pour ne pa saturer le navigateur
+    totalBonbon = 0
     // let plateau = document.querySelector(".plateau"); // déclare la variable plateau et selectionne la classe plateau un peu comme si j'avais ramener la div plateau ici
     for (let i in grille) {  // boucle sur toutes les lignes du tableau 
         for (let j in grille[i]) {  // boucles sur toutes les colonnes d'une ligne
-            console.log(grille[i][j]); // permet d'afficher dans la console 
+            // console.log(grille[i][j]); // permet d'afficher dans la console 
             let monElement = document.createElement("div"); // créer une nouvelle div
             if (grille[i][j] == 0) {
                 monElement.className = "mur"; // donne une valeur mur a la div si 0
@@ -62,6 +70,7 @@ function afficheGrille() {
 
             if (grille[i][j] == 2) {
                 monElement.className = "bonbon"; // donne une valeur mur a la div si 2
+                totalBonbon++
             }
             monElement.style.gridArea = (+i + 1) + "/" + (+j + 1)
             plateau.appendChild(monElement); // après avoir créer mes div (monElement) je les envoie dans la div plateau
@@ -73,8 +82,21 @@ function afficheGrille() {
 
 function tourDeJeu() {
     afficheGrille()
+    afficheScore()
+    movePacman()
+
+    collision()
+    mange()
+    depasse()
+
     affichePacman()
-    afficheGhost()
+    winner()
+
+
+
+
+
+    // afficheGhost()
 };
 
 function affichePacman() {
@@ -94,6 +116,86 @@ function afficheGhost() {
 
 function movePacman() {
     if (pacman.direction == 0) {
-        pacman.x
+        pacman.x++
     }
+    if (pacman.direction == 1) {
+        pacman.y++
+    }
+    if (pacman.direction == 2) {
+        pacman.x--
+    }
+    if (pacman.direction == 3) {
+        pacman.y--
+    }
+}
+
+function collision() {
+    if (grille[pacman.y - 1][pacman.x - 1] == 0) {
+        if (pacman.direction == 0) {
+            pacman.x--
+        }
+        if (pacman.direction == 1) {
+            pacman.y--
+        }
+        if (pacman.direction == 2) {
+            pacman.x++
+        }
+        if (pacman.direction == 3) {
+            pacman.y++
+        }
+    }
+}
+
+function clavier(event) {
+
+    if (event.key == "ArrowDown") {
+        pacman.direction = 1
+    }
+    if (event.key == "ArrowUp") {
+        pacman.direction = 3
+    }
+    if (event.key == "ArrowRight") {
+        pacman.direction = 0
+    }
+    if (event.key == "ArrowLeft") {
+        pacman.direction = 2
+    }
+}
+
+function depasse() {
+    if (pacman.y < 0) {
+        pacman.y = 21
+    }
+    if (pacman.x < 0) {
+        pacman.x = 19
+    }
+    if (pacman.y > 21) {
+        pacman.y = 0
+    }
+    if (pacman.x > 19) {
+        pacman.x = 0
+    }
+}
+
+function mange() {
+    if (grille[pacman.y - 1][pacman.x - 1] == 2) {
+        grille[pacman.y - 1][pacman.x - 1] = 1
+        score += 10
+    }
+}
+
+function afficheScore() {
+    let divscore = document.querySelector(".score")
+    divscore.innerHTML = "Votre score est de :" + score;
+}
+
+function winner() {
+    if (totalBonbon == 0) {
+        alert("Vous avez gagné !!")
+        gameStop()
+    }
+}
+
+function gameStop() {
+    clearInterval(stop)
 }
